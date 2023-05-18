80ed16452535ee9ae169a54180d21d35e77206b8
erickp-paz <ericknathan.paz@gmail.com>
2023-05-18 17:37:29 -0300

    style: add TabBar icons

--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createDrawerNavigator } from '@react-navigation/drawer'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Ionicons } from '@expo/vector-icons'
 
 import HomeScreen from '../../src/screens/HomeScreen'
 import DiceRollScreen from '../../src/screens/DiceRollScreen'
@@ -31,7 +32,28 @@ const AppNavigation = () => {
 
 const MainTabs = () => {
   return (
-    <Tab.Navigator initialRouteName="Home">
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName
+
+          if (route.name === 'Dados') {
+            iconName = focused ? 'square' : 'square-outline'
+          } else if (route.name === 'Mestre') {
+            iconName = focused ? 'person' : 'person-outline'
+          } else if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline'
+          } else if (route.name === 'Ficha') {
+            iconName = focused ? 'file-tray-full' : 'file-tray-full-outline'
+          } else if (route.name === 'Criar') {
+            iconName = focused ? 'add-circle' : 'add-outline'
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />
+        }
+      })}
+    >
       <Tab.Screen name="Dados" component={DiceRollScreen} />
       <Tab.Screen name="Mestre" component={GameMasterScreen} />
       <Tab.Screen name="Home" component={HomeScreen} />
